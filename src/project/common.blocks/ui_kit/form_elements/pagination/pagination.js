49899364dd1c52494742fa4403e6c10abf9ba6af
DenBakh <den.bah89@gmail.com>
2020-05-25 23:43:33 +0700

    Разобрался с pagination, увидел, что для динамических элементов события обрабатываются иначе

--- src/project/common.blocks/ui_kit/form_elements/pagination/pagination.js
+++ src/project/common.blocks/ui_kit/form_elements/pagination/pagination.js
@@ -2,9 +2,9 @@ import '../pagination/jquery.simplePagination.js'
 
 $(document).ready(function () {
 	$(function () {
-		var $this = $('#pagination-demo2');
+		var $this = $('#pagination-main');
 		$this.pagination({
-			items: 180,
+			items: 176,
 			itemsOnPage: 12,
 			cssStyle: 'light-theme',
 			displayedPages: 3,
@@ -13,9 +13,8 @@ $(document).ready(function () {
 			nextText: 'next',
 			ellipsePageSet: false,
 		});
-		console.log($('#pagination-demo2').pagination('getItems'));
 
-
+		//Обновляем общее количество элементов на страницах
 		var $pagination = $this.closest('.pagination')
 		var $pagination__total = $pagination.find('.pagination__total-items')
 		var $items__total = $this.pagination('getItems')
@@ -24,19 +23,22 @@ $(document).ready(function () {
 	});
 
 
+	//Обновляем общее количество элементов на страницах
+	$(document).on('click', '.page-link', function (e) {
 
-	$('.page-link').on('click', function (e) {
+		var $this = $('#pagination-main');
+		var $pagination = $this.closest('.pagination')
+		var $items__total = $this.pagination('getItems')
 
-
-		var $this = $('#pagination-demo2');
-
-		console.log(1)
 		var $items__view = $this.pagination('getItemsOnPage')
 		var $currentPage = $this.pagination('getCurrentPage')
 		var $pagination__view = $pagination.find('.pagination__current-items')
 		var $view__start = 1 + ($items__view * ($currentPage - 1))
 		var $view__end = $items__view * $currentPage
-		$pagination__view.text($view__start + ' - ' + $view__end)
+		if ($view__end * 1 > $items__total * 1) {
+			$view__end = $items__total
+		}
+		$pagination__view.text($view__start + ' - ' + $view__end + ' ')
 
 	});
 
