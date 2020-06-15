bd2677d61be5127b01cab2bdf3d533b14107d943
DenBakh <den.bah89@gmail.com>
2020-06-15 15:21:18 +0700

    Переделал pagination, добавил логику переключения данных на страницах, добавил скролл к началу данных на странице

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -9,6 +9,7 @@ import '~/project/common.blocks/ui_kit/form_elements/range-slider/range-slider.j
 import '~/project/common.blocks/ui_kit/form_elements/datepicker/datepicker.js'
 import '~/project/common.blocks/ui_kit/form_elements/option-buttons/checkbox/checkbox-simple/spoiler.js'
 import '~/project/common.blocks/ui_kit/cards/slider/slider.js'
+import '~/project/common.blocks/ui_kit/form_elements/pagination/pagination.js'
 
 
 
--- src/project/common.blocks/ui_kit/form_elements/pagination/pagination.js
+++ src/project/common.blocks/ui_kit/form_elements/pagination/pagination.js
@@ -1,34 +1,29 @@
 import '../pagination/jquery.simplePagination.js'
 
 $(document).ready(function () {
-	$(function () {
-		var $this = $('#pagination-main');
-		$this.pagination({
-			items: 176,
-			itemsOnPage: 12,
-			cssStyle: 'light-theme',
-			displayedPages: 3,
-			edges: 1,
-			prevText: '',
-			nextText: 'next',
-			ellipsePageSet: false,
-		});
-
-		//Обновляем общее количество элементов на страницах
-		var $pagination = $this.closest('.pagination')
-		var $pagination__total = $pagination.find('.pagination__total-items')
-		var $items__total = $this.pagination('getItems')
-		$pagination__total.text(Math.trunc($items__total / 100) * 100 + '+ ')
-
-	});
-
 
 	//Обновляем общее количество элементов на страницах
 	$(document).on('click', '.page-link', function (e) {
-
 		var $this = $('#pagination-main');
+		//определяем общее количество элементов и выводим в текст
+		pagination_parameters($this);
+		scroll_top();
+	});
+
+	function scroll_top() {
+		var scroll_point = $('#scroll-here');
+		if (scroll_point.length) {
+			var $top = scroll_point.offset().top; // получаем координаты блока
+			$('body, html').animate({ scrollTop: $top }, 800); // плавно переходим к блоку
+		}
+	};
+
+
+	function pagination_parameters($this) {
 		var $pagination = $this.closest('.pagination')
 		var $items__total = $this.pagination('getItems')
+		var $pagination__total = $pagination.find('.pagination__total-items')
+		$pagination__total.text(Math.trunc($items__total / 10) * 10 + '+ ')
 
 		var $items__view = $this.pagination('getItemsOnPage')
 		var $currentPage = $this.pagination('getCurrentPage')
@@ -39,10 +34,71 @@ $(document).ready(function () {
 			$view__end = $items__total
 		}
 		$pagination__view.text($view__start + ' - ' + $view__end + ' ')
+	};
 
+	$(function () {
+		var $this = $('#pagination-main');
+
+		// Consider adding an ID to your table
+		// incase a second table ever enters the picture.
+		var items = $(".pagination-item");
+		var numItems = items.length;
+		var perPage = 12;
+		// Only show the first 2 (or first `per_page`) items initially.
+		items.slice(perPage).hide();
+
+		// Now setup the pagination using the `.pagination-page` div.
+		$this.pagination({
+			items: numItems,
+			itemsOnPage: perPage,
+			cssStyle: 'light-theme',
+			displayedPages: 3,
+			edges: 1,
+			prevText: '',
+			nextText: 'next',
+			ellipsePageSet: false,
+
+			// This is the actual page changing functionality.
+			onPageClick: function (pageNumber) {
+				// We need to show and hide `tr`s appropriately.
+				var showFrom = perPage * (pageNumber - 1);
+				var showTo = showFrom + perPage;
+				// We'll first hide everything...
+				items.hide()
+					// ... and then only show the appropriate rows.
+					.slice(showFrom, showTo).show();
+			}
+		});
+
+		//определяем общее количество элементов и выводим в текст
+		pagination_parameters($this);
+		// EDIT: Let's cover URL fragments (i.e. #page-3 in the URL).
+		// More thoroughly explained (including the regular expression) in:
+		// https://github.com/bilalakil/bin/tree/master/simplepagination/page-fragment/index.html
+
+		// We'll create a function to check the URL fragment
+		// and trigger a change of page accordingly.
+		function checkFragment() {
+			// If there's no hash, treat it like page 1.
+			var hash = window.location.hash || "#page-1";
+
+			// We'll use a regular expression to check the hash string.
+			hash = hash.match(/^#page-(\d+)$/);
+
+			if (hash) {
+				// The `selectPage` function is described in the documentation.
+				// We've captured the page number in a regex group: `(\d+)`.
+				$this.pagination("selectPage", parseInt(hash[1]));
+			}
+		};
+
+		// We'll call this function whenever back/forward is pressed...
+		$(window).bind("popstate", checkFragment);
+
+		// ... and we'll also call it when the page has loaded
+		// (which is right now).
+		checkFragment();
 	});
-
-
 
 });
 
