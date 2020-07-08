371f980b127fc80daf57e1ae738a8be99dfd25f3
DenBakh <den.bah89@gmail.com>
2020-07-08 13:55:55 +0700

    Добавил в сборку оптимизацию изображений

--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -279,7 +279,32 @@ module.exports = {
 							name: '[name].[ext]',
 							esModule: false
 						}
+					},
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							mozjpeg: {
+								progressive: true,
+								quality: 100
+							},
+							// optipng.enabled: false will disable optipng
+							optipng: {
+								enabled: false,
+							},
+							pngquant: {
+								quality: [0.65, 0.90],
+								speed: 4
+							},
+							gifsicle: {
+								interlaced: false,
+							},
+							// the webp option will enable WEBP
+							webp: {
+								quality: 75
+							}
+						}
 					}
+
 				]
 			},
 
